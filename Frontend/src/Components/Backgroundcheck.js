072b45cf640021c62bfc7d5e2c59f0ce8aefb6ba
bharathbutta <83398144+bharathbutta@users.noreply.github.com>
2023-04-28 11:25:32 +0530

    Update Backgroundcheck.js

--- Frontend/src/Components/Backgroundcheck.js
+++ Frontend/src/Components/Backgroundcheck.js
@@ -2,10 +2,11 @@ import React from 'react'
 import Navbar from './Navbar'
 import Cookies from 'js-cookie'
 import BannerBackground from "../Assets/home-banner-background.png";
-import Employeepng from "../Assets/Credit assesment-amico.png";
+import Employeepng from "../Assets/Internship-amico.png";
+import AboutBackground from "../Assets/about-background.png";
 import { render } from "react-dom";
 
-// Import react-circular-progressbar module and styles
+
 import {
   CircularProgressbar,
   CircularProgressbarWithChildren,
@@ -16,20 +17,7 @@ import "react-circular-progressbar/dist/styles.css";
 
 
 
-// function Example(props) {
-//   return (
-//     <div>
-//       {/* <hr style={{ border: "2px solid #ddd" }} /> */}
-//       <div style={{ marginTop: 50}}>
-//         <div style={{  paddingRight: 30 }}>{props.children}</div>
-//         <div >
-//           <h2 className="background">Safe</h2>
-//           {/* <p>{props.description} </p> */}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
+
 
 function Example(props) {
   
@@ -60,15 +48,12 @@ const Backgroundcheck = () => {
     
     <div >
       <Navbar></Navbar>
-      {/* <div className="home-banner-container">
-        <div className="home-bannerImage-container">
-          <img src={BannerBackground} alt="" />
-        </div>
-        </div> */}
+
         <div >
-        <div className="backgroundcheck-container">
-          <img src={BannerBackground} alt="" />
-        </div>
+
+              <div className="backgroundcheck-container">
+        <img src={AboutBackground} alt="" />
+      </div>
       <h2 className='bglabel'>Background Check</h2>
       <div style={{ padding: "40px 40px 40px 40px" }}>
     <Example>
